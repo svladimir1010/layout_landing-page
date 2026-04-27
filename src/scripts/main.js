4bf9617ed3475b2328ce219ab08f28784adbc6fb
svladimir1010 <vladimireur@gmail.com>
2026-04-27 14:56:34 +0300

    feat: complete B&O landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const header = document.querySelector('.header');
+const menuBtn = document.querySelector('.header__menu-btn');
+const body = document.body;
+
+if (menuBtn && header) {
+  menuBtn.addEventListener('click', () => {
+    header.classList.toggle('is-open');
+
+    body.classList.toggle('no-scroll');
+  });
+}
