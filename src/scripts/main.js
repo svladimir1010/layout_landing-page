7376a0f8fb971a04528c3a20d4418b4f2a18b1aa
svladimir1010 <vladimireur@gmail.com>
2026-04-28 10:35:58 +0300

    feat: optimize images, add smooth scroll, comments and update README

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,20 @@
 const header = document.querySelector('.header');
 const menuBtn = document.querySelector('.header__menu-btn');
 const body = document.body;
+const mobileLinks = document.querySelectorAll('.mobile-menu a');
 
+// Перевіряємо, чи існують елементи перед додаванням слухачів
 if (menuBtn && header) {
   menuBtn.addEventListener('click', () => {
     header.classList.toggle('is-open');
-
     body.classList.toggle('no-scroll');
   });
 }
+
+// Закриваємо мобільне меню при кліку на будь-яке посилання
+mobileLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    header.classList.remove('is-open');
+    body.classList.remove('no-scroll');
+  });
+});
